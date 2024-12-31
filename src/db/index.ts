ea5202fdc3774c51ef6ec2c576b7e89c2b00904f
Kusal Kalinga <kusalkalingainfo@gmail.com>
2024-12-31 21:20:49 +0530

    hardcode value

--- src/db/index.ts
+++ src/db/index.ts
@@ -8,7 +8,9 @@ import { neon } from "@neondatabase/serverless";
 
 const url = process.env.DATABASE_URL;
 console.log("check db url ", url);
-const sql = neon(url!);
+const sql = neon(
+  "postgresql://neondb_owner:DtdXV9rKxA0U@ep-dark-rain-a1eiiejv.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
+);
 
 //logger
 
