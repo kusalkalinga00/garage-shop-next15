7179b56267f5f4d663cc9547cfb9a3f6ae5f8600
Kusal Kalinga <kusalkalingainfo@gmail.com>
2024-12-31 21:28:08 +0530

    remove hardcode value

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,16 +1,13 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-// import { config } from "dotenv";
+import { config } from "dotenv";
 
-// config({
-//   path: ".env.local",
-// });
+config({
+  path: ".env.local",
+});
 
 const url = process.env.DATABASE_URL;
-console.log("check db url ", url);
-const sql = neon(
-  "postgresql://neondb_owner:DtdXV9rKxA0U@ep-dark-rain-a1eiiejv.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
-);
+const sql = neon(url!);
 
 //logger
 
