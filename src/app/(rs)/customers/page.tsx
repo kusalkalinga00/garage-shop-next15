97e3a5569748ecff51868c85b4b626968777ad77
Kusal Kalinga <kusalkalingainfo@gmail.com>
2025-01-01 23:24:36 +0530

    navigation added and data queary from db added

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -1,7 +1,27 @@
+import CustomerSearch from "@/app/(rs)/customers/CustomerSearch";
+import { getCustomerSearchResults } from "@/lib/queries/getCustomerSearchResults";
+
 export const metadata = {
-  title: "Customers",
+  title: "Customers Search",
 };
 
-export default function Customers() {
-  return <h2>Customers Page</h2>;
+export default async function Customers({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+  const { searchText } = await searchParams;
+
+  if (!searchText) return <CustomerSearch />;
+
+  // query db
+  const results = await getCustomerSearchResults(searchText);
+
+  // return results
+  return (
+    <>
+      <CustomerSearch />
+      <p>{JSON.stringify(results)}</p>
+    </>
+  );
 }
